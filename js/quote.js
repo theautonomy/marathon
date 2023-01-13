143d67c2c82768daac0c8221b1ac3dd3de365fa0
Wei Li <weili.mail@gmail.com>
2023-01-12 22:19:44 -0500

    Trying animation

--- js/quote.js
+++ js/quote.js
@@ -3,7 +3,7 @@ export default {
     msg: String
   },
   template: `
-    <div class="container  mt-4 rounded">
+    <div id="moveIn" class="container  mt-4 rounded">
         <blockquote class="blockquote text-center font-weight-bold font-italic">
             <i>{{msg}}</i>
         </blockquote>
